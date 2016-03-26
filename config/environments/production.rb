c32d0300f74ee441452a808a5e45703b06ad281f
kyang6 <kyang6@stanford.edu>
2016-03-26 08:22:01 -0700

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,13 +2,14 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.paperclip_defaults = {
     storage: :s3,
-    bucket: ENV['AWS_BUCKET'],
+   
 
     s3_credentials: {
       bucket: ENV['AWS_BUCKET'],
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-    }
+    },
+    :url =>':s3_domain_url',
   }
   # Code is not reloaded between requests.
   config.cache_classes = true
