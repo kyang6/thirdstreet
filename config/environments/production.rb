770f5a68f7840364a3d3dbfd016c0ee427391a6f
kyang6 <kyang6@stanford.edu>
2016-03-26 08:13:02 -0700

    reoreder

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,8 +2,10 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.paperclip_defaults = {
     storage: :s3,
+    bucket: ENV['AWS_BUCKET'],
+    
     s3_credentials: {
-      bucket: ENV['AWS_S3_BUCKET'],
+     
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     }
